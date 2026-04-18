ca219c7c1f4c6dccd5c127c77625b737891afb33
hzh-betty <beidi_99@outlook.com>
2026-04-19 01:30:41 +0800

    test: update free_list.cc

--- zmalloc/src/free_list.cc
+++ zmalloc/src/free_list.cc
@@ -12,7 +12,7 @@
 namespace zmalloc {
 
 void FreeList::push(void *obj) {
-    assert(obj);
+    assert(obj); // GCOVR_EXCL_LINE
     // 头插法 O(1) 入链，适合高频小对象释放场景。
     next_obj(obj) = free_list_;
     free_list_ = obj;
@@ -20,7 +20,7 @@ void FreeList::push(void *obj) {
 }
 
 void *FreeList::pop() {
-    assert(free_list_);
+    assert(free_list_); // GCOVR_EXCL_LINE
     void *obj = free_list_;
     void *next = next_obj(free_list_);
     free_list_ = next;
@@ -31,7 +31,7 @@ void *FreeList::pop() {
 }
 
 void FreeList::push_range(void *start, void *end, size_t n) {
-    assert(start && end);
+    assert(start && end); // GCOVR_EXCL_LINE
     // 批量对象已在上层串成链，这里只做一次头拼接。
     next_obj(end) = free_list_;
     free_list_ = start;
@@ -44,7 +44,7 @@ void FreeList::pop_range(void *&start, void *&end, size_t n) {
         end = nullptr;
         return;
     }
-    assert(n <= size_);
+    assert(n <= size_); // GCOVR_EXCL_LINE
     start = free_list_;
     end = start;
     // 顺着单链走到第 n 个节点，拆出一段连续子链返回调用方。
@@ -57,11 +57,11 @@ void FreeList::pop_range(void *&start, void *&end, size_t n) {
 }
 
 size_t FreeList::pop_batch(void **batch, size_t n) {
-    assert(batch);
+    assert(batch); // GCOVR_EXCL_LINE
     if (n == 0) {
         return 0;
     }
-    assert(n <= size_);
+    assert(n <= size_); // GCOVR_EXCL_LINE
     void *cur = free_list_;
     for (size_t i = 0; i < n; ++i) {
         batch[i] = cur;
