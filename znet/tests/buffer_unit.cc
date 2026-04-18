b79d560853ad4f977ee32bb393f34adef78a0961
hzh-betty <beidi_99@outlook.com>
2026-04-18 20:32:40 +0800

    test: update buffer_unit.cc

--- znet/tests/buffer_unit.cc
+++ znet/tests/buffer_unit.cc
@@ -1,6 +1,16 @@
 #include "znet/buffer.h"
+#include "znet/socket.h"
+
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <string>
 
 #include <gtest/gtest.h>
+
+#include "zco/sched.h"
+#include "zco/wait_group.h"
 
 namespace znet {
 namespace {
@@ -31,6 +41,108 @@ TEST(BufferUnitTest, ReusesPrependSpaceWithoutCorruptingReadableData) {
 
     EXPECT_EQ(buffer.readable_bytes(), 7U);
     EXPECT_EQ(buffer.retrieve_all_as_string(), "678abcd");
+}
+
+TEST(BufferUnitTest, FindCrLfReturnsExpectedPointer) {
+    Buffer with_crlf;
+    with_crlf.append("abc\r\ndef", 8);
+    const char *found = with_crlf.find_crlf();
+    ASSERT_NE(found, nullptr);
+    EXPECT_EQ(std::string(with_crlf.peek(), found), "abc");
+
+    Buffer without_crlf;
+    without_crlf.append("abcdef", 6);
+    EXPECT_EQ(without_crlf.find_crlf(), nullptr);
+}
+
+TEST(BufferUnitTest, RetrieveHandlesOutOfRangeLength) {
+    Buffer buffer;
+    buffer.append("xyz", 3);
+    buffer.retrieve(10);
+    EXPECT_EQ(buffer.readable_bytes(), 0U);
+    EXPECT_EQ(buffer.peek(), nullptr);
+}
+
+TEST(BufferUnitTest, AppendNullOrEmptyInputIsNoop) {
+    Buffer buffer;
+    buffer.append(static_cast<const void *>(nullptr), 5);
+    buffer.append(static_cast<const char *>(nullptr), 5);
+    buffer.append("", 0);
+    EXPECT_EQ(buffer.readable_bytes(), 0U);
+}
+
+TEST(BufferUnitTest, ReadFromSocketValidatesArguments) {
+    Buffer buffer;
+    int saved_errno = 0;
+
+    errno = 0;
+    EXPECT_EQ(buffer.read_from_socket(nullptr, 16, 10, &saved_errno), -1);
+    EXPECT_EQ(errno, EBADF);
+    EXPECT_EQ(saved_errno, EBADF);
+
+    int pair[2] = {-1, -1};
+    ASSERT_EQ(::socketpair(AF_UNIX, SOCK_STREAM, 0, pair), 0);
+    auto socket = std::make_shared<Socket>(pair[0]);
+    ASSERT_NE(socket, nullptr);
+
+    errno = 0;
+    EXPECT_EQ(buffer.read_from_socket(socket, 0, 10, &saved_errno), -1);
+    EXPECT_EQ(errno, EINVAL);
+    EXPECT_EQ(saved_errno, EINVAL);
+
+    socket->close();
+    ::close(pair[1]);
+}
+
+TEST(BufferUnitTest, WriteToSocketHandlesInvalidAndEmptyCases) {
+    Buffer buffer;
+    int saved_errno = 0;
+
+    errno = 0;
+    EXPECT_EQ(buffer.write_to_socket(nullptr, 10, &saved_errno), -1);
+    EXPECT_EQ(errno, EBADF);
+    EXPECT_EQ(saved_errno, EBADF);
+
+    int pair[2] = {-1, -1};
+    ASSERT_EQ(::socketpair(AF_UNIX, SOCK_STREAM, 0, pair), 0);
+    auto socket = std::make_shared<Socket>(pair[0]);
+    ASSERT_NE(socket, nullptr);
+
+    EXPECT_EQ(buffer.write_to_socket(socket, 10, &saved_errno), 0);
+
+    socket->close();
+    ::close(pair[1]);
+}
+
+TEST(BufferUnitTest, ReadAndWriteSocketPathWorksInCoroutineContext) {
+    zco::init(1);
+
+    int pair[2] = {-1, -1};
+    ASSERT_EQ(::socketpair(AF_UNIX, SOCK_STREAM, 0, pair), 0);
+    auto reader = std::make_shared<Socket>(pair[0]);
+    ASSERT_NE(reader, nullptr);
+
+    Buffer input;
+    Buffer output;
+    output.append("hello", 5);
+
+    zco::WaitGroup done(1);
+    zco::go([&]() {
+        int saved_errno = 0;
+        EXPECT_EQ(::send(pair[1], "hello", 5, 0), 5);
+        EXPECT_EQ(input.read_from_socket(reader, 5, 200, &saved_errno), 5);
+        char recvbuf[8] = {0};
+        EXPECT_EQ(output.write_to_socket(reader, 200, nullptr), 5);
+        EXPECT_EQ(::recv(pair[1], recvbuf, sizeof(recvbuf), 0), 5);
+        EXPECT_STREQ(recvbuf, "hello");
+        done.done();
+    });
+    done.wait();
+
+    EXPECT_EQ(input.retrieve_all_as_string(), "hello");
+    reader->close();
+    ::close(pair[1]);
+    zco::shutdown();
 }
 
 } // namespace
