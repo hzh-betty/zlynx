8110d8dab75c484fa174488c3b2a7155c417c521
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:05 +0800

    test(zhttp): 按批次提交文件

--- zhttp/tests/unit/http_common_test.cc
+++ zhttp/tests/unit/http_common_test.cc
@@ -135,7 +135,8 @@ TEST(HttpCommonTest, UrlDecodeHandlesValidAndInvalidEscapes) {
 } // namespace zhttp
 
 int main(int argc, char **argv) {
-    zhttp::init_logger();
     ::testing::InitGoogleTest(&argc, argv);
+
+    zhttp::init_logger();
     return RUN_ALL_TESTS();
 }
