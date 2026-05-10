1b84b958d6e4ac739666c18f6b3528feaef5ed11
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交文件

--- zco/tests/unit/snapshot_buffer_pool_test.cc
+++ zco/tests/unit/snapshot_buffer_pool_test.cc
@@ -118,5 +118,6 @@ TEST_F(SnapshotBufferPoolUnitTest, ReleaseNullBufferIsNoop) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
