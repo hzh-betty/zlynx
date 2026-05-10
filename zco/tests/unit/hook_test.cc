fb11747094080dca06bb9f09471249227a99ed10
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交单文件：zco/tests/unit/hook_test.cc（超过150行）

--- zco/tests/unit/hook_test.cc
+++ zco/tests/unit/hook_test.cc
@@ -264,5 +264,6 @@ TEST_F(HookUnitByHeaderTest, OutsideCoroutineIoHooksFailWithEperm) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
