d75fd5868394ef4b21f8c729b7d801a914d71cc5
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交单文件：zco/tests/unit/io_event_test.cc（超过150行）

--- zco/tests/unit/io_event_test.cc
+++ zco/tests/unit/io_event_test.cc
@@ -184,5 +184,6 @@ TEST_F(IoEventUnitTest, CoroutineZeroTimeoutWithoutReadyEventReturnsEtimedout) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
