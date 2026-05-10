4c967febace2e7bf4059d225fc236c4ed3ae4564
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交文件

--- zco/tests/unit/fiber_pool_test.cc
+++ zco/tests/unit/fiber_pool_test.cc
@@ -67,5 +67,6 @@ TEST_F(FiberPoolUnitTest, ZeroCapacityPoolRejectsRecycle) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
