86fa46f77559c167aad6882afd614371b3eebe3b
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交单文件：zco/tests/unit/runtime_manager_test.cc（超过150行）

--- zco/tests/unit/runtime_manager_test.cc
+++ zco/tests/unit/runtime_manager_test.cc
@@ -625,5 +625,6 @@ TEST_F(RuntimeManagerUnitTest, WaitFdRejectsZeroEventsInsideCoroutine) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
