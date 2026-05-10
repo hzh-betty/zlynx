1f25c4e9c5bab7fc6410905a9afa6da2e60b2abf
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交单文件：zco/tests/unit/epoller_test.cc（超过150行）

--- zco/tests/unit/epoller_test.cc
+++ zco/tests/unit/epoller_test.cc
@@ -380,5 +380,6 @@ TEST_F(EpollerUnitTest, ConsumeWakeupFdNoopWhenWakeFdInvalid) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
