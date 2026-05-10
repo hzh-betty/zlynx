ee2418fbf421285c7c159cff46db6da3ba060d4c
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:05 +0800

    test(zhttp): 按批次提交单文件：zhttp/tests/unit/error_middleware_test.cc（超过150行）

--- zhttp/tests/unit/error_middleware_test.cc
+++ zhttp/tests/unit/error_middleware_test.cc
@@ -171,7 +171,7 @@ TEST(ErrorMiddlewareTest, EscapesAllControlCharactersInMessageAndPath) {
 }
 
 int main(int argc, char **argv) {
-    zhttp::init_logger();
     ::testing::InitGoogleTest(&argc, argv);
+    zhttp::init_logger();
     return RUN_ALL_TESTS();
 }
