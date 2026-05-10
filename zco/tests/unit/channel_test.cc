cfb9d1c9ff0cd3badf75d940050c7cdc1a3ce89e
hzh-betty <beidi_99@outlook.com>
2026-05-10 15:05:04 +0800

    test(zco): 按批次提交文件

--- zco/tests/unit/channel_test.cc
+++ zco/tests/unit/channel_test.cc
@@ -124,5 +124,6 @@ TEST_F(ChannelUnitByHeaderTest, CoroutineProducerConsumerWorks) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    zco::init_logger();
     return RUN_ALL_TESTS();
 }
